7a2049a0848a5e1bb237a160d44b82fc7390bd70
Danielle Glick <danielle@Danielles-MacBook-Pro.local>
2018-12-15 21:28:14 -0800

    restructure of the data model

--- variants/models.py
+++ variants/models.py
@@ -14,15 +14,43 @@ class Transcript(models.Model):
     Initial model for transcripts
     """
     transcript_id = models.AutoField(primary_key=True)
-    transcript = models.CharField(max_length=256, blank=True)
+    name = models.CharField(max_length=32, blank=True)
 
 
-class OtherMappings(models.Model):
+class Allele(models.Model):
+    allele_id = models.AutoField(primary_key=True)
+    allele = models.CharField(max_length=16, blank=True)
+
+
+class Mapping(models.Model):
     """
     Model for other mappings.
     """
-    other_mappings_id = models.AutoField(primary_key=True)
-    other_mapping = models.TextField(blank=True)
+    mapping_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, blank=True)
+
+
+class Variant(models.Model):
+    """
+    Initial model for variants.
+    """
+    variant_id = models.AutoField(primary_key=True)
+    mappings = models.ManyToManyField(
+        Mapping,
+        through="MappingJoinTable",
+        through_fields=('variant', 'mapping'),
+    )
+    ref_allele = models.ForeignKey(Allele, on_delete=models.PROTECT, related_name="ref_allele")
+    alt_allele = models.ForeignKey(Allele, on_delete=models.PROTECT, related_name="alt_allele")
+    reported_ref = models.ForeignKey(Allele, on_delete=models.PROTECT, related_name="reported_ref")
+    reported_alt = models.ForeignKey(Allele, on_delete=models.PROTECT, related_name="reported_alt")
+    protein_change = models.CharField(max_length=32, blank=True)
+
+
+class MappingJoinTable(models.Model):
+    mapping = models.ForeignKey(Mapping, on_delete=models.PROTECT)
+    variant = models.ForeignKey(Variant, on_delete=models.PROTECT)
+    nucleotide_change = models.BooleanField()
 
 
 class GenomicLocation(models.Model):
@@ -32,22 +60,20 @@ class GenomicLocation(models.Model):
     genomic_location_id = models.AutoField(primary_key=True)
     assembly = models.CharField(max_length=8, blank=True)
     chromosome = models.CharField(max_length=8, blank=True)
-    start = models.CharField(max_length=24, blank=True)
-    stop = models.CharField(max_length=24, blank=True)
-    accession = models.CharField(max_length=24, blank=True)
+    start = models.CharField(max_length=32, blank=True)
+    stop = models.CharField(max_length=32, blank=True)
+    transcripts = models.ManyToManyField(
+        Transcript,
+        through="TranscriptJoinTable",
+        through_fields=('genomic_location', 'transcript'),
+    )
+    region = models.CharField(max_length=64, blank=True)
 
 
-class Variant(models.Model):
-    """
-    Initial model for variants.
-    """
-    variant_id = models.AutoField(primary_key=True)
-    nucleotide_change = models.CharField(max_length=24, blank=True)
-    protein_change = models.CharField(max_length=24, blank=True)
-    ref_allele = models.CharField(max_length=24, blank=True)
-    alt_allele = models.CharField(max_length=24, blank=True)
-    reported_ref = models.CharField(max_length=24, blank=True)
-    reported_alt = models.CharField(max_length=24, blank=True)
+class TranscriptJoinTable(models.Model):
+    transcript = models.ForeignKey(Transcript, on_delete=models.PROTECT)
+    genomic_location = models.ForeignKey(GenomicLocation, on_delete=models.PROTECT)
+    accession = models.BooleanField()
 
 
 class Source(models.Model):
@@ -55,9 +81,9 @@ class Source(models.Model):
     Model for Variant Source information.
     """
     source_id = models.AutoField(primary_key=True)
-    source = models.CharField(max_length=24, blank=True)  # Note: Have not noticed any empty fields here, but not unique
-    last_eval = models.CharField(max_length=24, blank=True)
-    last_updated = models.CharField(max_length=24, blank=True)
+    source = models.CharField(max_length=16, blank=True)  # Note: Have not noticed any empty fields here, but not unique
+    last_eval = models.CharField(max_length=16, blank=True)
+    last_updated = models.CharField(max_length=16, blank=True)
     source_url = models.URLField(blank=True)
 
 
@@ -66,13 +92,16 @@ class Classification(models.Model):
     Model for Variant Classification
     """
     classification_id = models.AutoField(primary_key=True)
-    reported = models.CharField(max_length=256, blank=True)
-    inferred = models.CharField(max_length=256, blank=True)
+    # Max currently length 58.
+    name = models.CharField(max_length=128, blank=True)
 
 
 class ExtraProperties(models.Model):
     """
-    Model to add extra properties
+    Model to add extra properties.
+
+    These seem to be uncommon, but have significant text when present. Breaking out into
+    seperate table for size.
     """
     extra_props_id = models.AutoField(primary_key=True)
     submitter_comments = models.TextField(blank=True)
@@ -87,10 +116,10 @@ class GeneVariantInfo(models.Model):
     # To serve as an identifier for each entry since none of the fields are true identifier fields
     uuid_for_variant = models.CharField(max_length=48)
     gene = models.ForeignKey(Gene, on_delete=models.PROTECT)
-    transcript = models.ForeignKey(Transcript, on_delete=models.PROTECT)
+    # many to many
     variant = models.ForeignKey(Variant, on_delete=models.PROTECT)
     genomic_location = models.ForeignKey(GenomicLocation, on_delete=models.PROTECT)
     source = models.ForeignKey(Source, on_delete=models.PROTECT)
-    classification = models.ForeignKey(Classification, on_delete=models.PROTECT)
-    other_mappings = models.ForeignKey(OtherMappings, on_delete=models.PROTECT)
+    reported_classification = models.ForeignKey(Classification, on_delete=models.PROTECT, related_name="reported")
+    inferred_classification = models.ForeignKey(Classification, on_delete=models.PROTECT, related_name="inferred")
     extra_properties = models.ForeignKey(ExtraProperties, on_delete=models.PROTECT)
--- variants/tests/test_models.py
+++ variants/tests/test_models.py
@@ -1,7 +1,8 @@
+import unittest
 from django.test import TestCase
 
-from variants.models import Gene, Transcript, GenomicLocation, OtherMappings, Variant, Source, Classification, \
-    ExtraProperties, GeneVariantInfo
+from variants.models import Gene, Transcript, Allele, GenomicLocation, Mapping, Variant, Source, Classification, \
+    ExtraProperties, GeneVariantInfo, MappingJoinTable, TranscriptJoinTable
 
 
 class GeneModelCreateTests(TestCase):
@@ -18,73 +19,109 @@ class GeneModelCreateTests(TestCase):
 
 class TranscriptModelCreateTests(TestCase):
     def test_add_transcript(self):
-        Transcript.objects.get_or_create(transcript='NM_003159.2')
+        Transcript.objects.get_or_create(name='NM_003159.2')
         last_entry = Transcript.objects.last()
-        self.assertEqual(last_entry.transcript, 'NM_003159.2')
+        self.assertEqual(last_entry.name, 'NM_003159.2')
 
     def test_add_transcript_blank(self):
-        Transcript.objects.get_or_create(transcript='')
+        Transcript.objects.get_or_create(name='')
         last_entry = Transcript.objects.last()
-        self.assertEqual(last_entry.transcript, '')
+        self.assertEqual(last_entry.name, '')
 
 
 class GenomicLocationModelCreateTests(TestCase):
     def test_add_genomic_location(self):
-        GenomicLocation.objects.get_or_create(assembly='GRCh37', chromosome='1', start='25598276', stop='25655538',
-                                              accession='NC_000001.10')
-        last_entry = GenomicLocation.objects.last()
-        self.assertEqual(last_entry.assembly, 'GRCh37')
-        self.assertEqual(last_entry.accession, 'NC_000001.10')
+        """
+        CM000663.1, \
+        NC_000001.11, \
+        NC_000001.10
+        """
+        transcript1, created = Transcript.objects.get_or_create(name='CM000663.1')
+        transcript2, created = Transcript.objects.get_or_create(name='NC_000001.11')
+        transcript3, created = Transcript.objects.get_or_create(name='NC_000001.10')
+
+        genome_loc, created = GenomicLocation.objects.get_or_create(assembly='GRCh37', chromosome='1', start='25598276',
+                                                                    stop='25655538', region="")
+
+        last_genomic_loc = GenomicLocation.objects.last()
+        self.assertEqual(last_genomic_loc.assembly, 'GRCh37')
+        self.assertEqual(last_genomic_loc.start, '25598276')
+
+        TranscriptJoinTable.objects.get_or_create(transcript=transcript1, genomic_location=genome_loc,
+                                                  accession=False)
+        TranscriptJoinTable.objects.get_or_create(transcript=transcript2, genomic_location=genome_loc,
+                                                  accession=True)
+        TranscriptJoinTable.objects.get_or_create(transcript=transcript3, genomic_location=genome_loc,
+                                                  accession=False)
+        transcripts_for_genomic_loc = TranscriptJoinTable.objects.filter(genomic_location__genomic_location_id=
+                                                                         genome_loc.genomic_location_id)
+        self.assertEqual(len(transcripts_for_genomic_loc), 3)
+        self.assertEqual(transcripts_for_genomic_loc[0].transcript.name, 'CM000663.1')
 
     def test_add_genomic_location_blank(self):
         GenomicLocation.objects.get_or_create(assembly='', chromosome='', start='', stop='',
-                                              accession='')
+                                              region='')
         last_entry = GenomicLocation.objects.last()
         self.assertEqual(last_entry.assembly, '')
-        self.assertEqual(last_entry.accession, '')
-
-    def test_add_genomic_location_blank_accession(self):
-        GenomicLocation.objects.get_or_create(assembly='GRCh37', chromosome='1', start='25598276', stop='25655538',
-                                              accession='')
-        last_entry = GenomicLocation.objects.last()
-        self.assertEqual(last_entry.assembly, 'GRCh37')
-        self.assertEqual(last_entry.accession, '')
+        self.assertEqual(last_entry.region, '')
 
 
-class OtherMappingsModelCreateTests(TestCase):
-    def test_add_other_mappings(self):
-        OtherMappings.objects.get_or_create(other_mapping='NM_003159.2:c.-162-?_99+?del')
-        last_entry = OtherMappings.objects.last()
-        self.assertEqual(last_entry.other_mapping, 'NM_003159.2:c.-162-?_99+?del')
+class MappingModelCreateTests(TestCase):
+    def test_add_mappings(self):
+        Mapping.objects.get_or_create(name='NM_003159.2:c.-162-?_99+?del')
+        last_entry = Mapping.objects.last()
+        self.assertEqual(last_entry.name, 'NM_003159.2:c.-162-?_99+?del')
 
-    def test_add_other_mapping_blank(self):
-        OtherMappings.objects.get_or_create(other_mapping='')
-        last_entry = OtherMappings.objects.last()
-        self.assertEqual(last_entry.other_mapping, '')
+    def test_add_mapping_blank(self):
+        Mapping.objects.get_or_create(name='')
+        last_entry = Mapping.objects.last()
+        self.assertEqual(last_entry.name, '')
 
 
 class VariantModelCreateTests(TestCase):
     def test_add_variant(self):
-        Variant.objects.get_or_create(nucleotide_change='NM_006493.2:c.835G>A', protein_change='p.Asp279Asn',
-                                      ref_allele='G', alt_allele='A', reported_ref='G', reported_alt='A')
-        last_entry = Variant.objects.last()
-        self.assertEqual(last_entry.nucleotide_change, 'NM_006493.2:c.835G>A')
-        self.assertEqual(last_entry.protein_change, 'p.Asp279Asn')
+        """
+        AJ132917.1: c.1151_1183del33, \
+        NM_004992.3: c.1151_1183del33, \
+        NM_001110792.1: c.1187_1219del33, \
+        """
+        mapping1 = Mapping.objects.create(name='AJ132917.1: c.1151_1183del33')
+        mapping2 = Mapping.objects.create(name='NM_004992.3: c.1151_1183del33')
+        mapping3 = Mapping.objects.create(name='NM_001110792.1: c.1187_1219del33')
+
+        ref_allele, created = Allele.objects.get_or_create(allele='G')
+        alt_allele, created = Allele.objects.get_or_create(allele='A')
+        reported_ref, created = Allele.objects.get_or_create(allele='G')
+        reported_alt, created = Allele.objects.get_or_create(allele='A')
+
+        self.assertEqual(ref_allele, reported_ref)
+        self.assertEqual(alt_allele, reported_alt)
+
+        variant = Variant.objects.create(ref_allele=ref_allele, alt_allele=alt_allele, reported_ref=reported_ref,
+                                         reported_alt=reported_alt, protein_change='p.Pro384_Ser395delinsArg')
+
+        MappingJoinTable.objects.get_or_create(mapping=mapping1, variant=variant, nucleotide_change=False)
+        MappingJoinTable.objects.get_or_create(mapping=mapping2, variant=variant, nucleotide_change=True)
+        MappingJoinTable.objects.get_or_create(mapping=mapping3, variant=variant, nucleotide_change=False)
+
+        last_variant = Variant.objects.last()
+        self.assertEqual(last_variant.protein_change, 'p.Pro384_Ser395delinsArg')
+
+        variant_mapping = MappingJoinTable.objects.filter(variant__variant_id=variant.variant_id)
+        self.assertEqual(len(variant_mapping), 3)
+        self.assertTrue(variant_mapping[1].nucleotide_change)
+        self.assertEqual(variant_mapping[2].mapping.name, 'NM_001110792.1: c.1187_1219del33')
 
     def test_add_variant_blank(self):
-        Variant.objects.get_or_create(nucleotide_change='', protein_change='', ref_allele='', alt_allele='',
-                                      reported_ref='', reported_alt='')
-        last_entry = Variant.objects.last()
-        self.assertEqual(last_entry.nucleotide_change, '')
-        self.assertEqual(last_entry.protein_change, '')
+        ref_allele, created = Allele.objects.get_or_create(allele='')
+        alt_allele, created = Allele.objects.get_or_create(allele='')
+        reported_ref, created = Allele.objects.get_or_create(allele='')
+        reported_alt, created = Allele.objects.get_or_create(allele='')
 
-    def test_add_variant_blank_ref_alt(self):
-        Variant.objects.get_or_create(nucleotide_change='NM_000093.4:c.3259_3366del',
-                                      protein_change='p.Ser1088_Gly1123del', ref_allele='', alt_allele='',
-                                      reported_ref='', reported_alt='')
+        Variant.objects.create(ref_allele=ref_allele, alt_allele=alt_allele, reported_ref=reported_ref,
+                               reported_alt=reported_alt, protein_change='')
         last_entry = Variant.objects.last()
-        self.assertEqual(last_entry.nucleotide_change, 'NM_000093.4:c.3259_3366del')
-        self.assertEqual(last_entry.ref_allele, '')
+        self.assertEqual(last_entry.protein_change, '')
 
 
 class SourceModelCreateTests(TestCase):
@@ -104,22 +141,21 @@ class SourceModelCreateTests(TestCase):
 
 class ClassificationModelCreateTests(TestCase):
     def test_add_classification(self):
-        Classification.objects.get_or_create(reported='Pathogenic', inferred='Pathogenic')
+        Classification.objects.create(name='Pathogenic')
         last_entry = Classification.objects.last()
-        self.assertEqual(last_entry.reported, 'Pathogenic')
+        self.assertEqual(last_entry.name, 'Pathogenic')
 
     def test_add_classification_blank(self):
-        Classification.objects.get_or_create(reported='', inferred='')
+        Classification.objects.create(name='')
         last_entry = Classification.objects.last()
-        self.assertEqual(last_entry.reported, '')
+        self.assertEqual(last_entry.name, '')
 
 
 class ExtraPropertiesModelCreateTests(TestCase):
     def test_add_extra_properties_comments(self):
-        ExtraProperties.objects.get_or_create(submitter_comments='The variant was identified prenatally and suggested '
-                                                                 'risk for Menkes disease, which did not however '
-                                                                 'develop.',
-                                              alias='')
+        ExtraProperties.objects.create(submitter_comments='The variant was identified prenatally and suggested risk '
+                                                          'for Menkes disease, which did not however develop.',
+                                       alias='')
         last_entry = ExtraProperties.objects.last()
         self.assertEqual(last_entry.submitter_comments, 'The variant was identified prenatally and suggested risk for '
                                                         'Menkes disease, which did not however develop.')
