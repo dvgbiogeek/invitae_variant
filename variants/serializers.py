3e7e1e7d17c1914d0b25d19559f599dd2acbd093
dvgbiogeek <dvgbiogeek@gmail.com>
2018-12-18 17:21:20 -0800

    Serializers added for foreign key relations

--- variants/serializers.py
+++ variants/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from variants.models import Gene, GeneVariantInfo, Mapping, Transcript, Variant, Allele
+from variants.models import Gene, GeneVariantInfo, Mapping, Transcript, Variant, Allele, GenomicLocation, Source, \
+    Classification, ExtraProperties
 
 
 class GeneSerializer(serializers.HyperlinkedModelSerializer):
@@ -28,21 +29,65 @@ class AlleleSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class VariantExtrasSerializer(serializers.HyperlinkedModelSerializer):
+    ref_allele = AlleleSerializer()
+    alt_allele = AlleleSerializer()
+    reported_ref = AlleleSerializer()
+    reported_alt = AlleleSerializer()
+
     class Meta:
         model = Variant
         fields = ('ref_allele', 'alt_allele', 'reported_ref', 'reported_alt', 'protein_change')
 
 
+class GenomicLocationSerializer(serializers.HyperlinkedModelSerializer):
+    assembly = serializers.StringRelatedField()
+    chromosome = serializers.StringRelatedField()
+    start = serializers.StringRelatedField()
+    stop = serializers.StringRelatedField()
+    region = serializers.StringRelatedField()
+
+    class Meta:
+        model = GenomicLocation
+        fields = ('assembly', 'chromosome', 'start', 'stop', 'region')
+
+
+class SourceSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Source
+        fields = ('source', 'last_eval', 'last_updated', 'source_url')
+
+
+class ClassificationSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Classification
+        fields = ('name',)
+
+
+class ExpertPropertiesSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = ExtraProperties
+        fields = ('submitter_comments', 'alias')
+
+
 class GeneVariantInfoSerializer(serializers.HyperlinkedModelSerializer):
     transcripts = TranscriptSerializer(read_only=True, many=True)
     mappings = MappingSerializer(read_only=True, many=True)
-    # variant_extras = VariantExtrasSerializer(allow_null=True)
+    variant_extras = VariantExtrasSerializer()
+    genomic_location = GenomicLocationSerializer()
+    source = SourceSerializer()
+    reported_classification = ClassificationSerializer()
+    inferred_classification = ClassificationSerializer()
+    extra_properties = ExpertPropertiesSerializer()
 
     class Meta:
         model = GeneVariantInfo
-        fields = ('gene', 'mappings', 'transcripts', )
-                  #  'variant_extras', 'genomic_location', 'source',
-                  #  'reported_classification', 'inferred_classification', 'extra_properties')
+        fields = ('gene', 'mappings', 'transcripts', 'variant_extras', 'genomic_location', 'source',
+                  'reported_classification', 'inferred_classification', 'extra_properties')
+                  #
+                  #    )
 
 
 
